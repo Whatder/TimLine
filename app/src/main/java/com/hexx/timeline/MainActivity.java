482558ea739d452784701870edd39c65388e7f37
Hexx95 <hexx95@yahoo.com>
2018-10-28 15:54:12 +0800

    toolbar init

--- app/src/main/java/com/hexx/timeline/MainActivity.java
+++ app/src/main/java/com/hexx/timeline/MainActivity.java
@@ -1,13 +1,24 @@
 package com.hexx.timeline;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mToolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN|View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+        getWindow().setStatusBarColor(Color.TRANSPARENT);
+        mToolbar.setFitsSystemWindows(true);
     }
 }

97574a39205f79f1321268c7bde826b7ebaa2bb4
Hexx <hjj@picbling.com>
2018-12-24 14:49:41 +0800

    upgrade:adapter封装

--- app/src/main/java/com/hexx/timeline/view/MainActivity.java
+++ app/src/main/java/com/hexx/timeline/view/MainActivity.java
@@ -2,13 +2,22 @@ package com.hexx.timeline.view;
 
 import android.content.Context;
 import android.content.Intent;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.hexx.timeline.R;
 import com.hexx.timeline.base.BaseActivity;
 import com.hexx.timeline.contract.IMainContract;
 import com.hexx.timeline.presenter.MainPresenter;
+import com.hexx.timeline.widget.recycler.CommonAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 
 import butterknife.BindView;
 import butterknife.OnClick;
@@ -17,6 +26,8 @@ public class MainActivity extends BaseActivity<MainPresenter> implements IMainCo
 
     @BindView(R.id.btn)
     Button mBtn;
+    @BindView(R.id.rv_time_list)
+    RecyclerView mRvTimeList;
 
     public static void start(Context context) {
         Intent starter = new Intent(context, MainActivity.class);
@@ -43,8 +54,40 @@ public class MainActivity extends BaseActivity<MainPresenter> implements IMainCo
         mPresenter.getInfo();
     }
 
+    List<String> data = new ArrayList<>();
+    CommonAdapter<String, Holder> adapter;
+
     @Override
     public void succ(String msg) {
         Toast.makeText(this, msg, Toast.LENGTH_LONG).show();
+        for (int i = 0; i < 10; i++) {
+            data.add(String.format(Locale.CHINA, "This is item %d", i));
+        }
+        if (adapter == null) {
+            mRvTimeList.setLayoutManager(new LinearLayoutManager(this));
+            adapter = new CommonAdapter<String, Holder>(this) {
+                @Override
+                public Holder getHolder(ViewGroup viewGroup) {
+                    return new Holder(viewGroup);
+                }
+            };
+            mRvTimeList.setAdapter(adapter);
+        }
+        adapter.add(data);
+
+    }
+
+    class Holder extends CommonAdapter.BaseHolder<String> {
+        @BindView(R.id.tv_content)
+        TextView mTvContent;
+
+        public Holder(ViewGroup viewGroup) {
+            super(MainActivity.this, R.layout.item_time_layout, viewGroup);
+        }
+
+        @Override
+        public void bindData(String data) {
+            mTvContent.setText(data);
+        }
     }
 }
---
+++ app/src/main/java/com/hexx/timeline/widget/recycler/CommonAdapter.java
@@ -0,0 +1,68 @@
+package com.hexx.timeline.widget.recycler;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.ButterKnife;
+
+/**
+ * Created by Hexx on 2018/12/20 11:53
+ * Desc：通用adapter
+ */
+public abstract class CommonAdapter<D, H extends CommonAdapter.BaseHolder<D>>
+        extends RecyclerView.Adapter<H> {
+
+    private Context mContext;
+    private List<D> mData;
+    private H mHolder;
+
+    public CommonAdapter(Context context) {
+        mContext = context;
+        mData = new ArrayList<>();
+    }
+
+    public abstract H getHolder(ViewGroup viewGroup);
+
+    public void add(List<D> data) {
+        int lastPosition = mData.size();
+        mData.addAll(data);
+        notifyItemRangeChanged(lastPosition, mData.size() - lastPosition);
+    }
+
+    @NonNull
+    @Override
+    public H onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        return mHolder = getHolder(viewGroup);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull H holder, int i) {
+        holder.bindData(mData.get(i));
+    }
+
+    @Override
+    public int getItemCount() {
+        return mData.size();
+    }
+
+    public abstract static class BaseHolder<D> extends RecyclerView.ViewHolder {
+        public BaseHolder(Context context, int ResId, ViewGroup viewGroup) {
+            this(LayoutInflater.from(context).inflate(ResId, viewGroup, false));
+        }
+
+        private BaseHolder(@NonNull View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+        }
+
+        public abstract void bindData(D data);
+    }
+
+}
